e17842dc4bee09137c95d3286e17870509c22a6a
Esther <esther.kondratieff@gmail.com>
2021-04-28 12:59:51 +0200

    main en commentaire et correction code html

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,4 +1,5 @@
-let blocPage = document.getElementById("bloc-page");
-    		let test = document.createElement("div");
-			test.setAttribute('class', 'rouge')
-			blocPage.appendChild(test);			
+function test2() {
+    let blocPage = document.getElementById("bloc-page");
+    let test = document.createElement("div");
+    test.setAttribute('class', 'rouge')
+    blocPage.appendChild(test);			}
