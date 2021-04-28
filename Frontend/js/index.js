d67acc77914b793b4d0e85eb423810370cf753e4
Esther <esther.kondratieff@gmail.com>
2021-04-28 17:47:27 +0200

    création d'une section

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,4 +1,10 @@
-function produit() {
-    let blocPage = document.getElementsByClassName("ours");
-    let section = document.createElement("section");
-}
+let main = document.getElementById("main");      //récupération de l'element id=main
+const section = document.createElement("section"); //création d'une section
+section.classList.add ("ours")                     // ajout de la class ours à la section
+main.appendChild(section);           // cette section est un enfant de main => définir sa position html
+
+
+let ours = document.getElementsByClassName("ours");      //récupération de l'element class= ours
+const div = document.createElement("div");              //création d'une div
+div.classList.add ("ours__info")                     // ajout de la class ours__info à la div
+ours.appendChild(div);           // cette div est un enfant de ours => définir sa position html
