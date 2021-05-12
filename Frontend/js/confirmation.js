400940a5c71f959d812646654d75e36455be6e6f
Estkon <esther.kondratieff@gmail.com>
2021-05-12 17:47:18 +0200

    script panier

---
+++ Frontend/js/confirmation.js
@@ -0,0 +1,29 @@
+// lien vers API
+let content = ''
+let main = document.getElementById("mainConfirmation");  
+fetch("http://localhost:3000/api/teddies/")
+.then(res => res.json())
+.then (data => {
+    data.forEach(ours =>{
+    content= 
+    ` <section class="Thanks">
+            <p class="violet">
+            Nous vous remercions pour votre achat sur Ornibear by Ornico !
+            </p>
+            <p class="orange">
+            Vous retrouverez ci-après un récapitulatif de votre commande.
+            </p>
+            <p class="violet">à bientôt !</p>
+    </section>
+
+    <section class="recapitulatif">
+        <h3>2. Récapitulatif de commande</h3>
+        <p>Identifiant de commande: XYZ36</p>
+        <p>Montant de la commande: 48€</p>
+    </section>
+`
+})
+mainConfirmation.innerHTML = content
+
+})
+.catch(err => console.log(err));
